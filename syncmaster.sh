83f174f4114b43179c76eaaf14c06c647878a969
Konstantinos Patronas <kpatronas@gmail.com>
2022-04-27 13:03:24 +0300

    Update syncmaster.sh

--- syncmaster.sh
+++ syncmaster.sh
@@ -7,9 +7,14 @@ function transfer
     local src=$1
     local trg=$2
     local bnd=$3
-    echo "STARTING - SOURCE: ${src} TARGET: ${trg} BANDWIDTH: ${bnd}"
-    rsync --ignore-existing --bwlimit=${BND} --progress --append --partial -vz -e 'ssh -p 22' ${src} ${trg}
-    echo "COMPLETED - SOURCE: ${src} TARGET: ${trg} BANDWIDTH: ${bnd}"
+    echo "`date +"%Y-%m-%dT%H:%M:%S"` - STAR - SOURCE: ${src} TARGET: ${trg} BAND: ${bnd}"
+    rsync --ignore-existing --bwlimit=${BND} --progress --append --partial -vz -e 'ssh -p 22' ${src} ${trg} >/dev/null
+    RSYNC_STATUS=$?
+    if [ $RSYNC_STATUS -eq 0 ]; then
+        echo "`date +"%Y-%m-%dT%H:%M:%S"` - COMPLETED - SOURCE: ${src} TARGET: ${trg} BAND: ${bnd}"
+    else
+        echo "`date +"%Y-%m-%dT%H:%M:%S"` - FAILED - SOURCE: ${src} TARGET: ${trg} BAND: ${bnd}"
+    fi
 }
 
 export -f transfer
@@ -33,24 +38,24 @@ done
 # If parallel jobs less or equal to 0 then change value to 1
 if [ "$PARALLEL" -le "0" ];
 then
-    echo "Ambiguous number of parallel jobs given, reseting to 1"
+    echo "`date +"%Y-%m-%dT%H:%M:%S"` - Ambiguous number of parallel jobs given, reseting to 1"
     PARALLEL=1;
 fi
 
 # TRANSFERS must not be empty
 if [ -z "${TRANSFERS}" ];
 then
-    echo "Exiting: TRANSFERS is empty";
+    echo "`date +"%Y-%m-%dT%H:%M:%S"` - Exiting: TRANSFERS is empty";
     exit -1;
 fi
 
 # Verify that instance of script does not allready running
 if pidof -o %PPID -x "$0">/dev/null;
 then
-    echo "Exiting: An instance of the script is allready running."
+    echo "`date +"%Y-%m-%dT%H:%M:%S"` - Exiting: An instance of the script is allready running."
     exit -1;
 fi
 
 
 # Create and launch rsync jobs here
-echo "$TRANSFERS" | tr -s " " "\n" | tr -s ";" " " | xargs -n 3 -P ${PARALLEL} bash -c 'transfer $1 $2 $3' bash
+echo "$TRANSFERS" | tr -s " " "\n" | tr -s ";" " " | xargs -n 3 -P ${PARALLEL} bash -c 'transfer $1 $2 $3;true' bash
